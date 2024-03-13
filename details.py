81be46449d052da503fe0a2e2e07cd25db9d8e5d
hectorog6612 <159784158+hectorog6612@users.noreply.github.com>
2024-03-14 00:28:24 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 12606917
-api_hash = f25113b8c17dca6fa7abda53a86bd4f7
-bot_token = 6274323365:AAECpPF15IqBcIcgdAqzYFqv6H7UB0B1BGs
+api_id = 10608382
+api_hash = ea04136e7acb517f48ccf8425ca99243
+bot_token = 7102764761:AAF4dsvIX73-Wk1LjRD8zSWhXilM0kdC0Tw
 
